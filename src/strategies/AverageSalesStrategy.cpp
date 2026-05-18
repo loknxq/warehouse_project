79257ac398129518d0c4ac8ec8a6494ffac18d3c
loknxq <loknxq@yandex.ru>
2026-05-18 11:17:50 +0300

    .

--- src/strategies/AverageSalesStrategy.cpp
+++ src/strategies/AverageSalesStrategy.cpp
@@ -1,9 +1,8 @@
-// warehouse_project/src/strategies/AverageSalesStrategy.cpp
+
 #include "../../include/strategies/AverageSalesStrategy.h"
 
 int AverageSalesStrategy::calculate(int currentStock, int minStock,
                                      int avgDailySales, int deliveryDays) {
-    // Обработка отрицательных значений
     if (currentStock < 0) currentStock = 0;
     if (minStock < 0) minStock = 0;
     if (avgDailySales < 0) avgDailySales = 0;
